b3da8213801b2d87815f2ce5c4f8cda155940223
Amadou Sene <amadousene64@gmail.com>
2024-08-05 14:28:06 +0000

    Update the header component with user data

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
-import { Button, Navbar, TextInput } from 'flowbite-react'
+import { Avatar, Button, Dropdown, DropdownDivider, DropdownHeader, DropdownItem, Navbar, TextInput } from 'flowbite-react'
 import { Link, useLocation } from 'react-router-dom'
 import { AiOutlineSearch } from 'react-icons/ai'
 import { FaMoon } from 'react-icons/fa'
+import { useSelector } from 'react-redux'
 
 export default function Header() {
     const path = useLocation().pathname;
+    const {currentUser} = useSelector(state => state.user);
   return (
     <Navbar className='border-b-2'>
         <Link to="/" className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold
@@ -28,9 +30,30 @@ export default function Header() {
             <Button className='w-12 h-10 hidden sm:inline' color='gray' pill>
                 <FaMoon />
             </Button>
-            <Link to='/sign-in'>
-                <Button gradientDuoTone='purpleToBlue' outline>Se connecter</Button>
-            </Link>
+            { currentUser ? (
+                <Dropdown 
+                    arrowIcon = {false}
+                    inline
+                    label = {
+                        <Avatar alt='user' img={currentUser.profilPucture} rounded/>
+                    }
+                >
+                    <DropdownHeader>
+                        <span className='block text-sm'>@{ currentUser.username }</span>
+                        <span className='block text-sm font-medium truncate'>{ currentUser.email }</span>
+                    </DropdownHeader>
+                    <Link to={'/dashboard?tab=profile'}>
+                        <DropdownItem>Profil</DropdownItem>
+                    </Link>
+                    <DropdownDivider/>
+                    <DropdownItem>Se déconnecter</DropdownItem>
+                </Dropdown>
+            ): 
+            (
+                <Link to='/sign-in'>
+                    <Button gradientDuoTone='purpleToBlue' outline>Se connecter</Button>
+                </Link>
+            )} 
             <Navbar.Toggle/>
         </div>
         <Navbar.Collapse>
